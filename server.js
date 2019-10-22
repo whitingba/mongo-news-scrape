6cecb7261222e05d47ae96834c384987b537aa44
Barbara Whiting <whitingba@gmail.com>
2019-10-22 13:58:32 -0600

    scrape and get article routes completed

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 //Require node packages 
-var express = require('express');
-var handlebars = require('express-handlebars');
-//var logger = require('morgan');
-var mongoose = require('mongoose');
-var axios = require('axios');
-var cheerio = require('cheerio');
+let express = require('express');
+let handlebars = require('express-handlebars');
+//let logger = require('morgan');
+let mongoose = require('mongoose');
+let axios = require('axios');
+let cheerio = require('cheerio');
 
 
 //Require the models from my models folder
-var db = require('./models');
+let db = require('./models');
 
 //connection to port
-var PORT = 8080;
+const PORT = 8080;
 
 //Initialize Express
-var app = express();
+let app = express();
 
 //Use morgan logger for logging requests
 //app.use(logger('dev'));
@@ -29,10 +29,45 @@ mongoose.connect('mongodb://localhost/articledb', { useNewUrlParser: true });
 
 //Routes
 //GET route to scrap website of choice
+app.get('/scrape', function (req, res) {
+    //grab body of HTML using axios
+    axios.get('https://medium.com/topic/programming').then(function (response) {
+        //loan into cheerio and save it into a shorthand selector of $
+        let $ = cheerio.load(response.data);
+        //create an empty array to save the data to be scraped
+        let results = [];
 
+        $('div h3').each(function (i, element) {
+
+            results.title = $(this)
+                .children('a')
+                .text();
+            results.link = $(this)
+                .children('a')
+                .attr('href');
+
+            db.Article.create(result)
+                .then(function (dbArticle) {
+                    console.log(dbArticle);
+                })
+                .catch(function (err) {
+                    console.log(err);
+                });
+        });
+        res.send("Scrape Complete");
+    });
+});
 
 //Route to get the articles form the db
-
+app.get('articles', function (req, res) {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
 //Route to get a specific article by id and populate it with a note
 
